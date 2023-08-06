a6d6f3b61451fbafee7b474469ebc318a5e32797
RajKKapadia <raajforyou@gmail.com>
2023-08-06 16:12:17 +0530

    added num variations and size in the UI

--- gradio_ui.py
+++ gradio_ui.py
@@ -12,14 +12,15 @@ load_dotenv(find_dotenv())
 
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
-def generate_variations(image: Image) -> list[str]:
+
+def generate_variations(image: Image, n: float, size: str) -> list[str]:
     image_id = uuid.uuid1()
     image_path = f'{tempfile.gettempdir()}/{image_id}.png'
     image.save(image_path)
     response = openai.Image.create_variation(
         image=open(image_path, 'rb'),
-        n=2,
-        size='1024x1024'
+        n=int(n),
+        size=size
     )
     image_variations = []
     for data in response['data']:
@@ -32,7 +33,9 @@ def generate_variations(image: Image) -> list[str]:
 
 demo = gr.Interface(
     fn=generate_variations,
-    inputs=gr.components.Image(label='Input', type='pil'),
+    inputs=[gr.components.Image(label='Input', type='pil'),
+            gr.components.Number(label='Number of variations'),
+            gr.components.Dropdown(choices=['1024x1024', '512x512', '256x256'], label='Choose the image size')],
     outputs=gr.components.Gallery(label='Variations'),
     allow_flagging='never'
 )
